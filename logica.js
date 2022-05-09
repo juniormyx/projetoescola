dae03e5b2290530adb641e3f239e79b816e02056
Junior Paulo <junior.paulo74@yahoo.com>
2022-05-09 16:09:56 -0300

    Alterado a logica no arquivo logica.js e retirado uma linha da tag script no html

--- logica.js
+++ logica.js
@@ -1,13 +1,20 @@
+function esconderResult() {
+    document.getElementById("result").style.visibility = 'hidden';
+    document.getElementById("result").style.visibility = 'visible';
+
+}
+
 function calcularMedia() {
 
-    var n1 = parseInt(document.getElementById("primeiraNota").value)
-    var n2 = parseInt(document.getElementById("segundaNota").value)
-    var n3 = parseInt(document.getElementById("terceiraNota").value)
-    var n4 = parseInt(document.getElementById("quartaNota").value)
-
-    var media = parseFloat((n1 + n2 + n3 + n4) / 4)
-
-    result = parseFloat(document.getElementById("result"))
+    var n1 = Number(document.getElementById("primeiraNota").value)
+    var n2 = Number(document.getElementById("segundaNota").value)
+    var n3 = Number(document.getElementById("terceiraNota").value)
+    var n4 = Number(document.getElementById("quartaNota").value)
+    
+    media = Number((n1 + n2 + n3 + n4) / 4)
+    
+    document.getElementById("result").innerText = media
+    
 }
 
 calcularMedia()
